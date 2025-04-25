e45995acc9a2bd2b7e5dbc5a8da93d464cdf6bed
Computador Intelecto 1 <aluno1Intelecto@gmail.com>
2025-04-25 12:05:28 -0300

    att

--- js/script.js
+++ js/script.js
@@ -3,10 +3,23 @@
     videos.forEach(video => {
       video.addEventListener('mouseenter', () => {
         video.play();
+        console.log('ativei play');
       });
 
       video.addEventListener('mouseleave', () => {
         video.pause();
         video.currentTime = 0; // Reinicia o vídeo do começo
+        console.log('ativei pause');
       });
     });
+
+    //Abrir o display de videos 
+    const displayVideos = document.getElementById('videos');
+    const botaoVideos = document.getElementById('botao');
+
+    botaoVideos.addEventListener('click', function(){      
+      let displayAtualVideos = window.getComputedStyle(displayVideos);
+      if(displayAtualVideos.display === 'none'){
+        displayVideos.style.display = 'grid';
+      }
+    })
